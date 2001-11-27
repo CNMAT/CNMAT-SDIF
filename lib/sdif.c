932ab84baef4078178ade38b55174a3e7c33a4dd
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2001-11-27 18:27:29 +0000

    Originally from /disk41/matt-backup/sdif/ircam-latest/amar

--- lib/sdif.c
+++ lib/sdif.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 Regents of the University of California.
+ * Copyright (c) 1997,1998 Regents of the University of California.
  * All rights reserved.
  * The name of the University may not be used to endorse or promote
  * products derived from this software without specific prior written
@@ -8,120 +8,516 @@
  * IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE.
 
- sdif.h
+ sdif.c
 
  Utilities for formatting data into SDIF
 
+ SDIF spec: http://www.cnmat.berkeley.edu/SDIF
+
  Matt Wright, 1/24/97
+ Version 1.1 5/12/97 by Matt + Amar (little endian)
+ Version 1.2 1/12/98 by Amar (revised spec)
+
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "sdif.h"
 
-void FillSDIFGlobalHeader(struct SDIFGlobalHeader *h, int4 size) {
-    Copy4Bytes(h->FORM, "FORM");
-    Copy4Bytes(h->SDIF, "SDIF");
-    h->size = size;
+void SizeofSanityCheck(void) {
+  int OK = 1;
+  
+  if (sizeof(int32) != 4) {
+    fprintf(stderr, "sizeof(int32) is %d!!!\n", sizeof(int32));
+    OK = 0;
+  }
+  
+  if (sizeof(float32) != 4) {
+    fprintf(stderr, "sizeof(float32) is %d!!!\n", sizeof(float32));
+    OK = 0;
+  }
+  
+  if (sizeof(float64) != 8) {
+    fprintf(stderr, "sizeof(float64) is %d!!!\n", sizeof(float64));
+    OK = 0;
+  }
+  
+  if (!OK) {
+    exit(-234);
+  }
+}
+
+
+void FillSDIFGlobalHeader(struct SDIFGlobalHeader *h) {
+  Copy4Bytes(h->SDIF, "SDIF");
+  h->size = 8;
+  memset (h->reserved,0,8);
 }
 
 void Copy4Bytes(char *target, const char *string) {
-    target[0] = string[0];
-    target[1] = string[1];
-    target[2] = string[2];
-    target[3] = string[3];
+  target[0] = string[0];
+  target[1] = string[1];
+  target[2] = string[2];
+  target[3] = string[3];
 }
 
 int str4eq(const char *this, const char *that) {
-    return this[0] == that[0] && this[1] == that[1] &&
-	this[2] == that[2] && this[3] == that[3];
+  return this[0] == that[0] && this[1] == that[1] &&
+    this[2] == that[2] && this[3] == that[3];
 }
 
-FILE *OpenSDIFWrite(char *filename, int size) {
-    struct SDIFGlobalHeader h;
-    FILE *result = fopen(filename, "w");
+FILE *OpenSDIFWrite(const char *filename) {
+  struct SDIFGlobalHeader h;
+  FILE *result;
 
-    if (result == NULL) return NULL;
-    FillSDIFGlobalHeader(&h, size);
-    if (fwrite(&h, sizeof(h), 1, result) != 1) {
-	fclose(result);
-	return NULL;
-    }
-    return result;
-}
-    
-int CloseSDIFWrite(FILE *f) {
-    /* To do:  We could use fseek() and ftell() to compute the size, then go
-       back and patch it in the header. */
+  SizeofSanityCheck();
 
-    return fclose(f);
-}
-
-FILE *OpenSDIFRead(char *filename, int *sizep) {
-    FILE *result = fopen(filename, "r");
-    char buf[4];
-    int size;
-
-    if (result == NULL) return NULL;
-
-    /* Now make sure the header is OK. */
-    if (fread(buf, sizeof(*buf), 4, result) != 4) goto lose;
-    if (!str4eq(buf, "FORM")) goto lose;
-    if (fread(&size, sizeof(size), 1, result) != 1) goto lose;
-    if (fread(buf, sizeof(*buf), 4, result) != 4) goto lose;
-    if (!str4eq(buf, "SDIF")) goto lose;
-
-    if (sizep != NULL) {
-	*sizep = size;
-    }
-    return result;
-
-lose:
-    fprintf(stderr, "Bad SDIF header in file %s\n", filename);
+  if ((result = fopen(filename, "wb")) == NULL) return NULL;
+  FillSDIFGlobalHeader(&h);
+  if (WriteSDIFGlobalHeader(&h, result) < 1) {
     fclose(result);
     return NULL;
+  }
+
+  return result;
+}
+
+int CloseSDIFWrite(FILE *f) {
+  fflush(f);
+  return fclose(f);
+}
+int WriteSDIFGlobalHeader(struct SDIFGlobalHeader *h, FILE *f) {
+#ifdef LITTLE_ENDIAN
+  if (write1(&(h->SDIF),4,f) != 4)
+    return -1;
+  if (write4(&(h->size),1,f) != 1)
+    return -1;
+  if (write1(&(h->reserved),8,f) != 8)
+    return -1;
+  return 1;
+#else
+  return fwrite(h, sizeof(*h), 1, f) == 1;
+#endif
+}
+
+
+FILE *OpenSDIFRead(const char *filename) {
+  FILE *result = NULL;
+  char buf[8];
+  int size;
+
+  SizeofSanityCheck();
+
+  if ((result = fopen(filename, "rb")) == NULL) return NULL;
+
+  /* Now make sure the header is OK. */
+  if (read1(buf,4, result) != 4) goto lose;
+  if (!str4eq(buf, "SDIF")) goto lose;
+  if (read4(&size, 1, result) != 1) goto lose;
+  
+  if (size % 8 != 0) {
+    goto lose;
+  }
+
+  /* skip size bytes */
+  fseek (result,size,SEEK_CUR);
+
+  return result;
+
+lose:
+  fprintf(stderr, "Bad SDIF header in file %s\n", filename);
+  fclose(result);
+  return NULL;
 }
 
 int CloseSDIFRead(FILE *f) {
-    fclose(f);
+  return fclose(f);
 }
 
-#include <sys/time.h>
+#include <time.h>
 
-int8 GenUniqueSDIFFrameID(void) {
-    int8 result;
-    struct timeval t;
-
-    if (gettimeofday(&t) != 0) {
-	fprintf(stderr, "gettimeofday() failed---Unique ID may not be.");
-	return 123456789;
-    }
-
-    result = t.tv_sec * 1000000 + t.tv_usec;
-    return result;
+int32 GenUniqueSDIFFrameID(void) {
+  return (int32) time(NULL);
 }
+
+
 
 int SkipSDIFFrame(struct SDIFFrameHeader *head, FILE *f) {
-    /* The header's size count includes the 8-byte time tag and 8-byte
-       ID that we already read. */
-    int bytesToSkip = head->size - 16;
+  /* The header's size count includes the 8-byte time tag, 4-byte
+     stream ID and 4-byte matrix count that we already read. */
+  int bytesToSkip = head->size - 16;
 
-    if (bytesToSkip < 0) {
-	fprintf(stderr, "Badly formed SDIF file: frame's size too low for time tag and ID.\n");
-	return -1;
-    }
+  if (bytesToSkip < 0) {
+    fprintf(stderr, "Badly formed SDIF file: frame's size too low for time tag and ID.\n");
+    return -1;
+  }
 
-    if (fseek(f, bytesToSkip, SEEK_CUR) != 0) {
-	fprintf(stderr, "fseek failed while skipping unrecognized frame type\n");	
-	return -2;
-    }
+  if (fseek(f, bytesToSkip, SEEK_CUR) != 0) {
+    fprintf(stderr, "fseek failed while skipping unrecognized frame type\n");
+    return -2;
+  }
 
-    return 0;
+  return 0;
 }
 
-int4 SizeOf1TRCFrame(int numTracks) {
-    /* 16 bytes for the time stamp and ID, plus 8 bytes for the # rows and #
-       columns, plus four 4-byte floating point numbers (index, freq, phase,
-       amp) for each track appearing in this frame. */
 
-    return 16 + 8 + (4 * 4 * numTracks);
+/***********************/
+
+int WriteSDIFFrameHeader(struct SDIFFrameHeader *fh, FILE *f) {
+#ifdef LITTLE_ENDIAN
+  if (write1(&(fh->frameType),4,f) != 4)
+    return -1;
+  if (write4(&(fh->size),1,f) != 1)
+    return -1;
+  if (write8(&(fh->time),1,f) != 1)
+    return -1;
+  if (write4(&(fh->streamID),1,f) != 1)
+    return -1;
+  if (write4(&(fh->matrixCount),1,f) != 1)
+    return -1;
+#ifdef __WIN32__
+  fflush(f);
+#endif
+  return 1;
+#else
+  return fwrite(fh, sizeof(*fh), 1, f) == 1;
+#endif
 }
+
+int ReadSDIFFrameHeader(struct SDIFFrameHeader *fh, FILE *f) {
+#ifdef LITTLE_ENDIAN
+  if (read1(&(fh->frameType),4,f) != 4)
+    return -1;
+  if (read4(&(fh->size),1,f) != 1)
+    return -1;
+  if (read8(&(fh->time),1,f) != 1)
+    return -1;
+  if (read4(&(fh->streamID),1,f) != 1)
+    return -1;
+  if (read4(&(fh->matrixCount),1,f) != 1)
+    return -1;
+  return 1;
+#else
+  return fread(fh, sizeof(*fh), 1, f) == 1;
+#endif
+}
+
+/***********/
+
+int WriteRowOf1TRC(RowOf1TRC *row, FILE *f) {
+  return (write4(row,4,f) == 4);
+}
+
+int ReadRowOf1TRC(RowOf1TRC *row, FILE *f) {
+  return read4(row,4,f) == 4;
+}
+
+int Read1TRCVals(FILE *f, float *indexp, float *freqp, float *phasep, float *ampp) {
+  RowOf1TRC data;
+
+#ifdef LITTLE_ENDIAN
+  if (read4(&data, 4, f) != 1) {
+    return -1;
+  }
+#else
+  if (fread (&data, sizeof(data), 1, f) != 1) {
+    return -1;
+  }
+#endif
+
+  *indexp = data.index;
+  *freqp = data.freq;
+  *phasep = data.phase;
+  *ampp = data.amp;
+  return 0;
+}
+
+int Write1TRCVals(FILE *f, float index, float freq, float phase, float amp) {
+  RowOf1TRC data;
+
+  data.index = index;
+  data.freq = freq;
+  data.phase = phase;
+  data.amp = amp;
+
+#ifdef LITTLE_ENDIAN
+  if (write4(&data, 4, f) != 1) {
+    return -1;
+  }
+#else
+  if (fwrite (&data, sizeof(data), 1, f) != 1) {
+    return -1;
+  }
+#endif
+
+  return 0;
+
+}
+
+int32 SizeOf1TRCFrame (int numTracks) {
+  /* 16 bytes for the time stamp, ID and matrix count, plus 16 bytes for
+     the # rows, # columns, matrix type and matrix data type,
+     plus four 4-byte floating point numbers (index, amp, freq, phase)
+     for each track appearing in this frame. Note that this is always a
+     multiple of 8, so no padding is necessary*/
+
+  return  16 + 16 + (4 * 4 * numTracks);
+}
+
+
+/************* 1RES ****************/
+
+int WriteRowOf1RES (RowOf1RES *row, FILE *f) {
+  return (write4(row,4,f) == 4);
+}
+
+int ReadRowOf1RES (RowOf1RES *row, FILE *f) {
+  return read4(row,4,f) == 4;
+}
+
+int32 SizeOf1RESFrame (int numResonances) {
+  /* 16 bytes for the time stamp, ID and matrix count, plus 16 bytes for
+     the # rows, # columns, matrix type and matrix data type,
+     plus four 4-byte floating point numbers (freq, gain, bw phase)
+     for each track appearing in this frame. Note that this is always a
+     multiple of 8, so no padding is necessary*/
+
+  return  16 + 16 + (4 * 4 * numResonances);
+}
+
+
+/***********/
+
+int WriteMatrixHeader(MatrixHeader *m, FILE *f) {
+#ifdef LITTLE_ENDIAN
+  if (write1(&(m->matrixType),4,f) != 4) return -1;
+  if (write4(&(m->matrixDataType),1,f) != 1) return -1;
+  if (write4(&(m->rowCount),1,f) != 1) return -1;
+  if (write4(&(m->columnCount),1,f) != 1) return -1;
+  return 1;
+#else
+  return fwrite(m, sizeof(*m), 1, f) == 1;
+#endif
+
+}
+int ReadMatrixHeader(MatrixHeader *m, FILE *f) {
+#ifdef LITTLE_ENDIAN
+  if (read1(&(m->matrixType),4,f) != 4) return -1;
+  if (read4(&(m->matrixDataType),1,f) != 1) return -1;
+  if (read4(&(m->rowCount),1,f) != 1) return -1;
+  if (read4(&(m->columnCount),1,f) != 1) return -1;
+  return 1;
+#else
+  return fread(m, sizeof(*m), 1, f) == 1;
+#endif
+
+}
+
+
+
+/***********************************************************/
+/********* Abstract away big endian/little endian in *******/
+/********* reading/writing 1, 2, 4, and 8 byte words. ******/
+/***********************************************************/
+
+#ifdef LITTLE_ENDIAN
+#define BUFSIZE 4096
+static	char	p[BUFSIZE];
+#endif
+
+
+int write1(void *block, size_t n, FILE *f)
+{
+  return fwrite (block,1,n,f);
+}
+
+int write2(void *block, size_t n, FILE *f)
+{
+#ifdef LITTLE_ENDIAN
+
+  short temp;
+  char  *q = block;
+  int	  i,m=2*n;
+
+  if ((n << 1) > BUFSIZE) {
+    int num = BUFSIZE >> 1;
+    int numWritten;
+    numWritten = write2(block, num, f);
+    numWritten += write2(((char *) block) + num, n-num, f);
+    return numWritten;
+  } 
+
+  for (i = 0; i < m; i += 2) {
+    p[i] = q[i+1];
+    p[i+1] = q[i];
+  }
+  return fwrite(p,2,n,f);
+#else
+  return fwrite (block,2,n,f);
+#endif
+}
+
+int write4(void *block, size_t n, FILE *f)
+{
+#ifdef LITTLE_ENDIAN
+  char  *q = block;
+  int	  i,m = 4*n;
+
+  if ((n << 2) > BUFSIZE) {
+    int num = BUFSIZE >> 2;
+    int numWritten;
+    numWritten = write4(block, num, f);
+    numWritten += write4(((char *) block) + num, n-num, f);
+    return numWritten;
+  } 
+
+  for (i = 0; i < m; i += 4) {
+    p[i] = q[i+3];
+    p[i+3] = q[i];
+    p[i+1] = q[i+2];
+    p[i+2] = q[i+1];
+  }
+  return fwrite(p,4,n,f);
+#else
+  return fwrite (block,4,n,f);
+#endif
+
+}
+
+int write8(void *block, size_t n, FILE *f)
+{
+#ifdef LITTLE_ENDIAN
+  char  *q = block;
+  int	  i,m = 8*n;
+
+  if ((n << 3) > BUFSIZE) {
+    int num = BUFSIZE >> 3;
+    int numWritten;
+    numWritten = write8(block, num, f);
+    numWritten += write8(((char *) block) + num, n-num, f);
+    return numWritten;
+  } 
+
+
+  for (i = 0; i < m; i += 8) {
+    p[i] = q[i+7];
+    p[i+7] = q[i];
+    p[i+1] = q[i+6];
+    p[i+6] = q[i+1];
+    p[i+2] = q[i+5];
+    p[i+5] = q[i+2];
+    p[i+3] = q[i+4];
+    p[i+4] = q[i+3];
+  }
+  return fwrite(p,8,n,f);
+#else
+  return fwrite (block,8,n,f);
+#endif
+}
+
+
+int read1(void *block, size_t n, FILE *f)
+{
+  return fread (block,1,n,f);
+}
+
+int read2(void *block, size_t n, FILE *f)
+{
+
+#ifdef LITTLE_ENDIAN
+  short temp;
+  char	*q = block;
+  int	  i,m = 2*n;
+  int result;
+
+  if ((n << 1) > BUFSIZE) {
+    int num = BUFSIZE >> 1;
+    int numread;
+    numread = read2(block, num, f);
+    numread += read2(((char *) block) + num, n-num, f);
+    return numread;
+  } 
+
+  result = fread(p,2,n,f);
+
+  for (i = 0; i < m; i += 2) {
+    q[i] = p[i+1];
+    q[i+1] = p[i];
+  }
+
+  return result;
+#else
+  return fread(block,2,n,f);
+#endif
+}
+
+int read4(void *block, size_t n, FILE *f)
+{
+#ifdef LITTLE_ENDIAN
+  long temp;
+  char	*q = block;
+  int	  i,m = 4 * n;
+  int result;
+
+  if ((n << 2) > BUFSIZE) {
+    int num = BUFSIZE >> 2;
+    int numread;
+    numread = read4(block, num, f);
+    numread += read4(((char *) block) + num, n-num, f);
+    return numread;
+  } 
+
+  result = fread(p,4,n,f);
+
+  for (i = 0; i < m; i += 4) {
+    q[i] = p[i+3];
+    q[i+3] = p[i];
+    q[i+1] = p[i+2];
+    q[i+2] = p[i+1];
+  }
+  return result;
+
+#else
+  return fread(block,4,n,f);
+#endif
+
+}
+
+int read8(void *block, size_t n, FILE *f)
+{
+#ifdef LITTLE_ENDIAN
+  long temp;
+  char	*q = block;
+  int	  i,m = 8 * n;
+  int result;
+
+  if ((n << 3) > BUFSIZE) {
+    int num = BUFSIZE >> 3;
+    int numread;
+    numread = read8(block, num, f);
+    numread += read8(((char *) block) + num, n-num, f);
+    return numread;
+  } 
+
+
+  result = fread(p,8,n,f);
+  for (i = 0; i < m; i += 8) {
+      q[i] = p[i+7];
+      q[i+7] = p[i];
+      q[i+1] = p[i+6];
+      q[i+6] = p[i+1];
+      q[i+2] = p[i+5];
+      q[i+5] = p[i+2];
+      q[i+3] = p[i+4];
+      q[i+4] = p[i+3];
+  }
+  return result;
+
+#else
+  return fread(block,8,n,f);
+#endif
+}
+
+
+
+
--- lib/sdif.h
+++ lib/sdif.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 1997 Regents of the University of California.
+ * Copyright (c) 1996, 1997, 1998 Regents of the University of California.
  * All rights reserved.
  * The name of the University may not be used to endorse or promote
  * products derived from this software without specific prior written
@@ -10,21 +10,53 @@
 
  sdif.h
 
- API for formatting data into SDIF
+ API for working with SDIF.
+
+ SDIF spec: http://www.cnmat.berkeley.edu/SDIF
 
  Matt Wright, 12/4/96
+ 5/12/97 version 1.1 by Matt and Amar, incorporating little-endian issues
+ 1/12/98 version 1.2 by Amar, conforms to revised SDIF spec
+
 */
 
 /* You need to #include <stdio.h> before this file, for FILE *. */
 
-/* Create 4-byte int and 4-byte and 8-byte float typedefs. */
 
-#define SGI
-#ifdef SGI
-    typedef int int4;
-    typedef float float4;
-    typedef double float8;
-    typedef long long int int8;
+#ifndef __SDIF_H
+#define __SDIF_H
+
+/* Create 4-byte and 8-byte int and float typedefs. */
+
+#ifdef __sgi
+    typedef int int32;
+    typedef float float32;
+    typedef double float64;
+#elif defined(__WIN32__) || defined(_WINDOWS)
+    #ifndef _WINDOWS_
+    #include <windows.h>
+    #endif 
+    typedef int	int32;
+    typedef float float32;
+    typedef double float64;
+#elif defined(__LINUX__)
+    typedef int int32;
+    typedef float float32;
+    typedef double float64;
+#else
+    /* These won't necessarily be the right size on any conceivable
+       platform, so you may need to change them by hand.  All calls to
+       OpenSDIFRead() and OpenSDIFWrite() perform a sanity check of the
+       sizes of these types, so if they're wrong you'll find out about it. */
+
+    typedef long int32;
+    typedef float float32;
+    typedef double float64;
+
+#endif
+
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 /***********************************************************/
@@ -32,27 +64,29 @@
 /***********************************************************/
 
 struct SDIFGlobalHeader {
-    char FORM[4];   /* Should be 'F', 'O', 'R', 'M' */
-    int4 size;	    /* # bytes of data, not including form or size. */
-    char SDIF[4];   /* Should be 'S', 'D', 'I', 'F' */
+    char  SDIF[4];     /* Should be 'S', 'D', 'I', 'F' */
+    int32 size;	       /* # of reserved bytes, not including form or size. */
+    char  reserved[8]; /* reserved for future use */
 };
 
-#define SIZE_UNKNOWN 0xffffffff	 /* Value for size when unknown or not given */
 
-/* Writes 'FORM', the given size, and 'SDIF' into the given SDIFHeader
+
+/* Writes 'SDIF' into the given SDIFHeader
    struct. */
-void FillSDIFGlobalHeader(struct SDIFGlobalHeader *h, int4 size);
+void FillSDIFGlobalHeader(struct SDIFGlobalHeader *h);
+int  WriteSDIFGlobalHeader(struct SDIFGlobalHeader *h, FILE *f);
+
 
 /* Utility to open a new SDIF file + write the global header. */
-FILE *OpenSDIFWrite(char *filename, int size);
+FILE *OpenSDIFWrite(const char *filename);
 int CloseSDIFWrite(FILE *f);
 
 /* Utility to open an SDIF file for reading and parse the header.
    Prints an error message (to stderr) and returns NULL if the header
    is wrong.  Advances the file pointer to the beginning of the first
-   frame.  If sizep is non-NULL, the size count from the SDIF header 
+   frame.  If sizep is non-NULL, the size count from the SDIF header
    is written into *sizep. */
-FILE *OpenSDIFRead(char *filename, int *sizep);
+FILE *OpenSDIFRead(const char *filename);
 int CloseSDIFRead(FILE *f);
 
 
@@ -61,15 +95,21 @@ int CloseSDIFRead(FILE *f);
 /***********************************************************/
 
 struct SDIFFrameHeader {
-    char frameType[4];	/* Should be a registered frame type */
-    int4 size;		/* # bytes in this frame, not including
-			   frameType or size */
-    float8 time;	/* Time corresponding to data chunk */
-    int8 ID;		/* Frames that go together have the same ID */
+    char frameType[4];	 /* Should be a registered frame type */
+    int32 size;		 /* # bytes in this frame, not including
+			    frameType or size */
+    float64 time;	 /* Time corresponding to data chunk */
+    int32   streamID;		 /* Frames that go together have the same ID */
+    int32   matrixCount;  /* Number of matrices in frame */
 };
 
-/* Return a unique ID number based on number of microseconds since 1970. */
-int8 GenUniqueSDIFFrameID(void);
+int WriteSDIFFrameHeader(struct SDIFFrameHeader *fh, FILE *f);
+int ReadSDIFFrameHeader(struct SDIFFrameHeader *fh, FILE *f);
+
+
+/* Return a unique streamID number.  (In this implementation, the number is based on
+number of microseconds since 1970.) */
+int32 GenUniqueSDIFFrameID(void);
 
 
 /* Assuming that you just read an SDIFFrameHeader from an open file and don't
@@ -85,38 +125,108 @@ int SkipSDIFFrame(struct SDIFFrameHeader *head, FILE *f);
 
 
 /***********************************************************/
+/********* Some stuff for particular frame types: **********/
+/***********************************************************/
+
+
+/************* 1TRC ****************/
+
+typedef struct {
+    float32 index; float32 amp; float32 freq; float32 phase;
+} RowOf1TRC;
+
+int WriteRowOf1TRC (RowOf1TRC *row, FILE *f);
+int ReadRowOf1TRC (RowOf1TRC *row, FILE *f);
+
+
+/* Read a row of 1TRC data from an open file, writing results into pointers
+   you pass as arguments.  Returns 0 if succesful, nonzero otherwise. */
+int Read1TRCVals(FILE *f, float32 *indexp, float32 *freqp, float32 *phasep,
+		 float32 *ampp);
+
+/* Write a row of 1TRC data to an open file.  Returns 0 if succesful, nonzero
+   otherwise. */
+int Write1TRCVals(FILE *f, float32 index, float32 freq, float32 phase, float32 amp);
+
+
+/* How big does the size count need to be in a frame of 1TRC? */
+/* (Assuming that the frame contains one matrix) */
+int32 SizeOf1TRCFrame(int numTracks);
+
+/************* 1RES ****************/
+
+typedef struct {
+   float32 freq, amp, bandwidth, phase;
+} RowOf1RES;
+
+int32 SizeOf1RESFrame(int numResonances);
+int WriteRowOf1RES (RowOf1RES *row, FILE *f);
+int ReadRowOf1RES (RowOf1RES *row, FILE *f);
+
+
+/***********************************************************/
 /******************** Matrix Data **************************/
 /***********************************************************/
 
 typedef struct {
-    int4 numRows;
-    int4 numCols;
-} MatrixSize;
+    char  matrixType[4];
+    int32 matrixDataType;
+    int32 rowCount;
+    int32 columnCount;
+} MatrixHeader;
+
+int WriteMatrixHeader(MatrixHeader *m, FILE *f);
+int ReadMatrixHeader (MatrixHeader *m, FILE *f);
+
+/* codes for data types used in matrices: float32, float64 */
+
+typedef enum {SDIF_FLOAT32 = 1, SDIF_FLOAT64 = 2} SDIFDataType;
 
 /***********************************************************/
 /******************* Utility Procedures ********************/
 /***********************************************************/
 
+/* Trivial utility function to check two 4-byte strings for equality.
+   Returns zero if strings differ, nonzero if they're the same. */
+int str4eq(const char *thisone, const char *thatone);
 
-/* Trivial utility procedure to copy 4 bytes (e.g., "FORM") from a string to a
-   4-byte char array */
+/* Trivial utility procedure to copy 4 bytes (e.g., "FORM") from a
+   string to a 4-byte char array */
 void Copy4Bytes(char *target, const char *string);
 
 
-/* Trivial utility function to check two 4-byte strings for equality.  
-   Returns zero if strings differ, nonzero if they're the same. */
-int str4eq(const char *this, const char *that);
-
-
-
 /***********************************************************/
-/********* Some stuff for particular frame types: **********/
+/********* Abstract away big endian/little endian in *******/
+/********* reading/writing 1, 2, 4, and 8 byte words. ******/
 /***********************************************************/
 
-typedef struct {
-    float index; float freq; float phase; float amp;
-} RowOf1TRC;
+#if defined(__WIN32__) || defined(_WINDOWS)
+#define LITTLE_ENDIAN  1
+#else 
+    /* Insert other checks for your architecture here if it's little endian. */
+#endif
+
+/* These procedures are all just like fwrite() except that the size of the
+   objects you're writing is determined by which function you call 
+   instead of an explicit argument.  Also, they do little-endian
+   conversion when necessary. */
+
+int write1(void *block, size_t n, FILE *f);
+int write2(void *block, size_t n, FILE *f);
+int write4(void *block, size_t n, FILE *f);
+int write8(void *block, size_t n, FILE *f);
 
 
-/* How big does the size count need to be in a frame of 1TRC? */
-int4 SizeOf1TRCFrame(int numTracks);
+/* Same, but like fread(). */
+
+int read1(void *block, size_t n, FILE *f);
+int read2(void *block, size_t n, FILE *f);
+int read4(void *block, size_t n, FILE *f);
+int read8(void *block, size_t n, FILE *f);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SDIF_H */
+
